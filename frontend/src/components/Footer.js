8162c692ccd8cb0b064a0f630df08ab0f1e8a2ca
EliseoGold <eliseogold23@gmail.com>
2021-07-29 13:24:49 +0300

    ye

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { Container ,Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+
+                        <Col className='text-center py-3'>
+                            Copyright &copy; Eliseo Gold & Liel Barouch 
+                        </Col>
+
+                </Row>
+
+
+
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
