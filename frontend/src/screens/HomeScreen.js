f045d02b1e4a860db4e805628c66b348377bd0e0
EliseoGold <eliseogold23@gmail.com>
2021-07-31 18:08:31 +0300

    3

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import products from '../products'
+import React,{useState,useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
+
+
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() =>{
+    const fetchProducts = async () =>{
+      const {data }= await axios.get('/api/products')
+
+        setProducts(data)
+
+    }
+    fetchProducts()
+
+  },[])
   return (
-    <>
+    
+    <div>
       <h1>latest porducts</h1>
 
       <Row>
@@ -15,7 +30,7 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
-    </>
+    </div>
   )
 }
 
